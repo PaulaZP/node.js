d2b0a25b619252d52758d4a4e5abaf5c1315db7a
PaulaZP <mzp1405mail.com>
2021-07-11 15:04:01 -0600

    se agrega clase 10

---
+++ clase-10/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+require('dotenv').config(); /// cargar variables de ambiente
+
+const HOSTNAME = process.env.HOSTNAME || 'localhost';
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, HOSTNAME, () => {
+    console.log(`Server running on ${HOSTNAME}:${PORT}`);
+})
